52349faab7bb4799d90f5b259184f064554c9eb2
CRISTOUD Mattéo <cristoud.matteo.lgm@gmail.com>
2021-02-03 09:35:07 +0100

    Affichage temperature + pression de 4 villes

--- temperatureVilles.py
+++ temperatureVilles.py
@@ -16,21 +16,24 @@ def get_temperature(ville):
     return float(requests.get(url).json()['main']['temp'])
 
 
+def get_pression(ville):
+    url="http://api.openweathermap.org/data/2.5/weather?q="+ville+",fr&units=metric&lang=fr&appid=0a73790ec47f53b9e1f2e33088a0f7d0"
+    return float(requests.get(url).json()['main']['pressure'])
 
-def set_temperature_bdd(ville, temperature):
+
+def set_temperature_bdd(ville, temperature, pression):
 
 
     #update dans la bdd
     cnx = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='bdd_temperaturesvilles')
     cursor = cnx.cursor()
-    update_temp = ("UPDATE temperaturevilles SET temperatures = (%s) WHERE ville = (%s)")
-    data = (temperature, ville)
+    update_temp = ("UPDATE temperaturevilles SET temperatures = (%s), pression = (%s) WHERE ville = (%s)")
+    data = (temperature, pression, ville)
     cursor.execute(update_temp, data)
     cnx.commit()
     cursor.close()
     cnx.close()
 
-    #set_bdd(ville, get_temperature(ville))
     print("fin exec set_temp_bdd")
 
 
@@ -43,8 +46,10 @@ def main():
     #set_temperature_bdd("rouen", get_temperature("rouen"))
 
     while 1:
-        for i in range(4):
-            set_temperature_bdd(villes[i], get_temperature(villes[i]))
+        for ville in villes:
+            set_temperature_bdd(ville, get_temperature(ville), get_pression(ville))
+
+            print(ville, get_temperature(ville), get_pression(ville))
         print(time.asctime(time.localtime(time.time())))
         time.sleep(300)
 
